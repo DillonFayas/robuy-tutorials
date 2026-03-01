2fec01ba795fe09f04dc5c9c2d49f04b3a810e1d
DillonFayas <dillonfayas@gmail.com>
2026-02-28 18:52:24 -0600

    Fixed plugin page and markdown loading

--- data/plugins.js
+++ data/plugins.js
@@ -1,8 +1,8 @@
 const plugins = [
   {
-    name: "Water Physics Plugin",
-    description: "Adds realistic hose physics.",
-    image: "/assets/images/plugin1.png",
-    link: "https://example.com/download"
+    name: "Poncake's Plugin",
+    description: "Standard plugin for use with all of Poncake's products.",
+    image: "/assets/images/PoncakeLogo.png",
+    link: "https://create.roblox.com/store/asset/10368363581/Poncakes-Plugin"
   }
 ];

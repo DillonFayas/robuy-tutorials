399a0a7a962119beab5193e02b1f83641af36d11
fayasd <fayasd@msoe.edu>
2026-03-02 11:19:53 -0600

    Added all products with blank tutorials

--- data/products.js
+++ data/products.js
@@ -16,5 +16,83 @@ const products = [
     slug: "fire-pole",
     description: "Full setup and usage tutorials.",
     image: "/assets/images/fire-pole.png"
+  },
+  {
+    name: "ATM System",
+    slug: "atm-system",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/atm-system.png"
+  },
+  {
+    name: "Auto-Eject Ports",
+    slug: "auto-eject-ports",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/auto-eject-ports.png"
+  },
+  {
+    name: "Automatic Fire Generator",
+    slug: "automatic-fire-generator",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/automatic-fire-generator.png"
+  },
+  {
+    name: "Bearcat Animation Running Boards",
+    slug: "bearcat-running-boards",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/bearcat-running-boards.png"
+  },
+  {
+    name: "Body/Dash Cameras",
+    slug: "body-dash-cameras",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/body-dash-cameras.png"
+  },
+  {
+    name: "Bollard Barriers",
+    slug: "bollard-barriers",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/bollard-barriers.png"
+  },
+  {
+    name: "Booking System",
+    slug: "booking-system",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/booking-system.png"
+  },
+  {
+    name: "Emergency Services Radio",
+    slug: "emergency-services-radio",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/emergency-services-radio.png"
+  },
+  {
+    name: "Handcuffs",
+    slug: "handcuffs",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/handcuffs.png"
+  },
+  {
+    name: "Standard Running Boards",
+    slug: "standard-running-boards",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/standard-running-boards.png"
+  },
+  {
+    name: "Team Changer",
+    slug: "team-changer",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/team-changer.png"
+  },
+  {
+    name: "Trunk System",
+    slug: "trunk-system",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/trunk-system.png"
+  },
+  {
+    name: "Vehicle Exhaust Extractor",
+    slug: "vehicle-exhaust-extractor",
+    description: "Full setup and usage tutorials.",
+    image: "/assets/images/vehicle-exhaust-extractor.png"
   }
 ];
---
+++ products/bollard-barriers/tutorial.md
@@ -0,0 +1,11 @@
+## Installing your [PRODUCT NAME]
+
+Download the asset. If you purchased through Robuy, it was sent to your DMs on Discord. If you purchased with Payhip, you can retrieve it from your email.
+
+Open a **blank** baseplate project in Roblox Studio and drop the file in.
+
+## Full Tutorial
+
+<div class="video">
+<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+</div>

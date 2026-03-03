30d58441a3547fba8bb06f2f960e55fc6af226f8
DillonFayas <dillonfayas@gmail.com>
2026-03-02 20:35:42 -0600

    Updated expand icon

--- assets/js/tutorialapp.js
+++ assets/js/tutorialapp.js
@@ -32,8 +32,13 @@ function buildSteps() {
             const expandButton = document.createElement("button");
             expandButton.className = "toggle-btn";
             expandButton.setAttribute("aria-expanded", "false");
-            expandButton.innerHTML = `<svg class="icon" viewBox="0 0 24 24" width="16" height="16">
-    <path d="M8 5l8 7-8 7" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            expandButton.innerHTML = `<svg class="icon" viewBox="0 0 24 24">
+    <path d="M8 5l8 7-8 7"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"/>
   </svg>`;
 
             const buttonWrapper = document.createElement("span");

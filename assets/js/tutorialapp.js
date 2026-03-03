415a11f5a18a0ac42e30e664bb696dc15c61d6be
DillonFayas <dillonfayas@gmail.com>
2026-03-02 20:21:33 -0600

    Updated expanded button css

--- assets/js/tutorialapp.js
+++ assets/js/tutorialapp.js
@@ -32,7 +32,9 @@ function buildSteps() {
             const expandButton = document.createElement("button");
             expandButton.className = "toggle-btn";
             expandButton.setAttribute("aria-expanded", "false");
-            expandButton.innerHTML = `<span class="icon"></span>`;
+            expandButton.innerHTML = `<svg class="icon" viewBox="0 0 24 24" width="16" height="16">
+    <path d="M8 5l8 7-8 7" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`;
 
             const buttonWrapper = document.createElement("span");
             buttonWrapper.className = "expand-button-wrapper";
